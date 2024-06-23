8249108ae96bb03111651b9a10fc8f6fb856da04
guyaldous1 <guyaldous@gmail.com>
2024-06-23 14:57:46 +0530

    try different import

--- functions/all-lists.mjs
+++ functions/all-lists.mjs
@@ -1,7 +1,18 @@
-import client from './shared/db';
+// import client from './shared/db';
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.MONGO_DB;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+    }
+});
+
 
 const allLists = async (req, context) => {
-
 
   try {
 
