876e06a30565d4b975acd91602b81606c02adf39
guyaldous1 <guyaldous@gmail.com>
2024-06-23 15:08:48 +0530

    back to the start

--- functions/all-lists.mjs
+++ functions/all-lists.mjs
@@ -1,21 +1,10 @@
 import client from './shared/db';
-// import { MongoClient, ServerApiVersion } from "mongodb";
-// const uri = process.env.MONGO_DB;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//     }
-// });
-
 
 const allLists = async (req, context) => {
+
   try {
 
-    const database = (await client).db('randolisto');
+    const database = client.db('randolisto');
     const lists = database.collection('lists');
 
     const allLists = await lists.find().toArray();
